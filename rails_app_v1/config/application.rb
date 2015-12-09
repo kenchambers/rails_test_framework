331a8fd11c3c9542ef30bcb6bedd243395b67644
chowderhead <ken@critiqueit.com>
2015-12-08 18:18:50 -0800

    modifies generator specs from default
    generate starter files for our application’s test suite (instead of the
    default TestUnit).
    
    http://everydayrails.com/2012/03/12/testing-series-rspec-setup.html

--- rails_app_v1/config/application.rb
+++ rails_app_v1/config/application.rb
@@ -8,6 +8,33 @@ Bundler.require(*Rails.groups)
 
 module RailsAppV1
   class Application < Rails::Application
+
+
+    config.generators do |g|
+      g.test_framework :rspec,
+
+        # specifies to generate a fixture for each model (using a Factory Girl factory, instead of an actual fixture)
+        :fixtures => true,
+
+        #says to skip generating view specs. I won’t cover them in this book; instead we’ll use request specs to test interface elements.
+        # :view_specs => false,
+
+        #skips generating specs for the helper files Rails generates with each controller. As your comfort level with RSpec improves, consider changing this option to true and testing these files.
+        :helper_specs => false,
+
+        # omits a spec file for your config/routes.rb file. If your application is simple, as the one in this book will be, you’re probably safe skipping these specs. As your application grows, however, and takes on more complex routing, it’s a good idea to incorporate routing specs.
+        :routing_specs => false,
+
+        :controller_specs => true,
+        :request_specs => true
+
+        #And finally, g.fixture_replacement :factory_girl tells Rails to generate factories instead of fixtures, and to save them in the spec/factories directory.
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
+
+
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
